9d5891177b523da46cdc1b666bb8b9d20a1c4b49
IvanDragolov <sinkara23@abv.bg>
2018-09-20 10:40:01 +0300

    new exercise(lab)

---
+++ Multidimensional Arrays/02. Sum Matrix Columns/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+namespace _02._Sum_Matrix_Columns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsAndColumns = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None).Select(int.Parse).ToArray();
+            int[,] matrix = new int[rowsAndColumns[0], rowsAndColumns[1]];
+            for (int rows = 0; rows < rowsAndColumns[0]; rows++)
+            {
+                var rowValues = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.None).Select(int.Parse).ToArray();
+                for (int columns = 0; columns < rowsAndColumns[1]; columns++)
+                {
+                    matrix[rows, columns] = rowValues[columns];
+                }
+            }
+
+            int sum = 0;
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int columns = 0; columns < matrix.GetLength(1); columns++)
+                {
+                    sum += matrix[0,columns];
+                }
+            }
+            Console.WriteLine(matrix.GetLength(0));
+            Console.WriteLine(matrix.GetLength(1));
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Stack_Queues/01.ReverseNumbersWithStack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ReverseNumbersWithStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+              .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+              .Select(int.Parse)
+              .ToArray();
+
+            var stack = new Stack<int>(input);
+
+            Console.WriteLine(string.Join(" ", stack));
+        }
+    }
+}
