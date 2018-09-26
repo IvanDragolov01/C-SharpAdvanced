840e012400a487b7f0365c3b7607ec642bd6027f
IvanDragolov <sinkara23@abv.bg>
2018-09-26 10:22:31 +0300

    new files

---
+++ Multidimensional Arrays/04.Maximal Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+namespace _04.Maximal_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rowsAndColumns = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[,] matrix = new int[rowsAndColumns[0], rowsAndColumns[1]];
+
+
+        }
+    }
+}
