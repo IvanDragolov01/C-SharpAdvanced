0193f31985d9c68f1ddb456d33c1a8beea2181c6
IvanDragolov <sinkara23@abv.bg>
2018-09-28 10:40:06 +0300

    update and add ex  5

--- Multidimensional Arrays/05. Rubiks Matrix/Program.cs
+++ Multidimensional Arrays/05. Rubiks Matrix/Program.cs
@@ -5,10 +5,6 @@ namespace _05._Rubiks_Matrix
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            int[] rowsAndColumns = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
-            int[,] matrix = new int[rowsAndColumns[0], rowsAndColumns[1]];
-        }
+        
     }
 }
