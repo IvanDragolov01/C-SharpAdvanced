4da6cccbec306e321d6494da085f26df73412980
IvanDragolov <sinkara23@abv.bg>
2018-10-03 11:41:27 +0300

    update

---
+++ Streams/Streams-Exercise/Ex1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Streams_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = new StreamReader("Ex1.cs"))
+            {
+                var lineNumber = 1;
+                 
+                string line;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    Console.WriteLine($"Line {lineNumber}: " + line);
+                    lineNumber++;
+                }
+            }
+        }
+    }
+}
