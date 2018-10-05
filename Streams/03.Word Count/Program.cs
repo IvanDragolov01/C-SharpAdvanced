fc45ef65115352d68b1a09697a66cf2c1b8c8af4
IvanDragolov <sinkara23@abv.bg>
2018-10-05 12:19:22 +0300

    ex 3

--- Streams/03.Word Count/Program.cs
+++ Streams/03.Word Count/Program.cs
@@ -6,9 +6,37 @@ namespace _03.Word_Count
     {
         static void Main(string[] args)
         {
-            using (var stream = new StreamReader(@"D:\CSharpAdvance\C-SharpAdvanced\Streams\Streams-Exercise\text.txt"))
+            using (var readStreamtext = new StreamReader(@"D:\CSharpAdvance\C-SharpAdvanced\Streams\Streams-Exercise\text.txt"))
             {
-
+                using (var readStreamword = new StreamReader(@"D:\CSharpAdvance\C-SharpAdvanced\Streams\03.Word Count\words.txt"))
+                {
+                    
+                    using (var writeStream = new StreamWriter(@"D:\CSharpAdvance\C-SharpAdvanced\Streams\02.Line Numbers\broi.txt"))
+                    {
+                        string a = readStreamword.ReadLine();
+                        string b = readStreamword.ReadLine();
+                        string c = readStreamword.ReadLine();
+                        int sum1 = 0, sum2 = 0, sum3 = 0;
+                        
+                        string line;
+                        while ((line = readStreamtext.ReadLine()) != null)
+                        {
+                            string[] input = line.Split(' ');
+                            for (int i = 0; i < input.Length; i++)
+                            {
+                                if (a == input[i]) sum1++;                             
+                                if (b == input[i]) sum2++;
+                                if (c == input[i]) sum3++;
+                            }
+                            
+                        }
+                        
+                        writeStream.WriteLine(a + "-" + sum1);
+                        writeStream.WriteLine(b + "-" + sum2);
+                        writeStream.WriteLine(c + "-" + sum3);
+                        
+                    }
+                }
             }
         }
     }
