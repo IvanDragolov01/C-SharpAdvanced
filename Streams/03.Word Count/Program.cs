a6a4773de497d34e1dd7ec4aa2154f1f40200e48
IvanDragolov <sinkara23@abv.bg>
2018-10-05 11:09:04 +0300

    add ex 3

---
+++ Streams/03.Word Count/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+namespace _03.Word_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = new StreamReader(@"D:\CSharpAdvance\C-SharpAdvanced\Streams\Streams-Exercise\text.txt"))
+            {
+
+            }
+        }
+    }
+}
