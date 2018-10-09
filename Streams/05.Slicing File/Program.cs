12656c223676750f4d6cb2e1654ea6dfceaee302
IvanDragolov <sinkara23@abv.bg>
2018-10-09 10:07:51 +0300

    add ex 3 4 5

---
+++ Streams/05.Slicing File/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace _05.Slicing_File
+{
+	class Program
+	{
+		private const int bufferSize = 4096;
+		static void Main(string[] args)
+		{
+
+			string sourceFile = @"D:\CSharpAdvance\C-SharpAdvanced\Streams\05.Slicing File\sliceMe.mp4";
+			string destination = "";
+			int parts = 5;
+
+			Slice(sourceFile, destination, parts);
+		}
+
+		static void Slice(string sourceFile, string destinationDirectory, int parts)
+		{
+			using (FileStream reader = new FileStream(sourceFile, FileMode.Open))
+
+			{
+				string extension = sourceFile.Substring(sourceFile.LastIndexOf('.') + 1);
+				long pieceSize = (long)Math.Ceiling((double)reader.Length / parts);
+				long currentPieceSize = 0;
+
+				for (int i = 0; i < parts; i++)
+				{
+
+					if (destinationDirectory == string.Empty)
+					{
+						destinationDirectory = "./";
+					}
+					string currentPart = destinationDirectory + $"Part-{i}.{extension}";
+					using (FileStream writer = new FileStream(currentPart, FileMode.Create))
+					{
+						byte[] buffer = new byte[bufferSize];
+						while (reader.Read(buffer, 0, bufferSize) == bufferSize)
+						{
+							writer.Write(buffer, 0, bufferSize);
+							currentPieceSize += bufferSize;
+							if (currentPieceSize >= pieceSize)
+							{
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Streams/Streams-Exercise/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+
+namespace Streams_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = new StreamReader(@"D:\CSharpAdvance\C-SharpAdvanced\Streams\Streams-Exercise\text.txt"))
+            {
+                var lineNumber = 1;
+                 
+                string line;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    if (lineNumber % 2  == 0)
+                    {
+                        
+                        Console.WriteLine(line);
+                        
+                    }
+                    lineNumber++;
+                }
+            }
+        }
+    }
+}
+/*
+using System;
+using System.IO;
+
+namespace Streams_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = new StreamReader(@"D:\CSharpAdvance\C-SharpAdvanced\Streams\Streams-Exercise\text.txt"))
+            {
+                var lineNumber = 0;
+
+                string line;
+                while ((line = stream.ReadLine()) != null)
+                {
+
+                    if (lineNumber % 3 != 0)
+                    {
+
+                        Console.WriteLine(line);
+                        break;
+
+                    }
+                    lineNumber++;
+
+                }
+            }
+        }
+    }
+}
+*/
+/*
+using System;
+using System.IO;
+
+namespace Streams_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = new StreamReader(@"D:\CSharpAdvance\C-SharpAdvanced\Streams\Streams-Exercise\text.txt"))
+            {
+                var lineNumber = 0;
+
+                string line;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    lineNumber++;
+                    if (lineNumber % 2 == 0)
+                    {
+
+                        Console.WriteLine(line);
+
+                    }
+
+                }
+            }
+        }
+    }
+}
+*/
