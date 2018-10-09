ab125429b5d80e633ba3e877e9749bab560bccef
IvanDragolov <sinkara23@abv.bg>
2018-10-09 11:13:19 +0300

    update

--- Streams/05.Slicing File/Program.cs
+++ Streams/05.Slicing File/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace _05.Slicing_File
 {
@@ -13,7 +15,18 @@ namespace _05.Slicing_File
 			string destination = "";
 			int parts = 5;
 
+			
 			Slice(sourceFile, destination, parts);
+
+			var files = new List<string>
+			{
+				"Part-0.mp4",
+				"Part-1.mp4",
+				"Part-2.mp4",
+				"Part-3.mp4",
+				"Part-4.mp4",
+			};
+			Assemble(files,destination);
 		}
 
 		static void Slice(string sourceFile, string destinationDirectory, int parts)
@@ -44,6 +57,38 @@ namespace _05.Slicing_File
 							{
 								break;
 							}
+						}
+					}
+				}
+			}
+		}
+
+		static void Assemble(List<string> files, string destinationDirectory)
+		{
+			string extension = files.First().Substring(files[0].LastIndexOf('.') + 1);
+
+			if (destinationDirectory == string.Empty)
+			{
+				destinationDirectory = @"D:\CSharpAdvance\C-SharpAdvanced\Streams\05.Slicing File\";
+			}
+			/*
+			 * if (!destinationDirectory.EndsWith("\"))
+			{
+				destinationDirectory += 
+			}
+			*/
+			string assembledFile = $"{destinationDirectory}Assembled.{extension}";
+
+			using (FileStream writer = new FileStream(assembledFile, FileMode.Create))
+			{
+				byte[] buffer = new byte[bufferSize];
+				foreach (var file in files)
+				{
+					using (FileStream reader = new FileStream(file, FileMode.Open))
+					{
+						while (reader.Read(buffer, 0 , bufferSize) == bufferSize)
+						{
+							writer.Write(buffer, 0, bufferSize);
 						}
 					}
 				}
