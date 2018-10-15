d5bc10ae651c13c28567fbd1c99d6c9362b4ad18
IvanDragolov <sinkara23@abv.bg>
2018-10-15 12:44:20 +0300

    add ex 1,2,3,4

--- Functional Programming/02. Knights of Honor/Program.cs
+++ Functional Programming/02. Knights of Honor/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Linq;
-
-namespace Concatenate_Data
+namespace _02._Knights_of_Honor
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Action<string> print = message => Console.WriteLine(message);
+			Action<string> print = message => Console.WriteLine("Sir " + message);
 			var name = Console.ReadLine().Split(' ').ToList();
 
 			foreach (var word in name)
---
+++ Functional Programming/03.Custom Min Function/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+namespace _03.Custom_Min_Function
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] ints = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+			Func<int[], int> minFunc = arr =>
+			{
+				int min = arr[0];
+				for (int i = 1; i < arr.Length; i++)
+				{
+					if (min > arr[i])
+					{
+						min = arr[i];
+					}
+				}
+				return min;
+			};
+
+			Console.WriteLine(minFunc(ints));
+		}
+	}
+}
