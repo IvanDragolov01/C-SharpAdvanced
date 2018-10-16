28be1c2676b328e5b6aa0b8343d4bc201709f86a
IvanDragolov <sinkara23@abv.bg>
2018-10-16 12:57:17 +0300

    add ex 7 and update ex 6

--- Functional Programming/06.Reserve and Exclude/Program.cs
+++ Functional Programming/06.Reserve and Exclude/Program.cs
@@ -6,7 +6,17 @@ namespace _06.Reserve_and_Exclude
 	{
 		static void Main(string[] args)
 		{
-			
+			int[] max = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+			int n = int.Parse(Console.ReadLine());
+			Func<int, int, int> isDivesible = (int x,int b) => x % b;
+			Action<int> print = x => Console.Write(x + " ");
+			for (int i = max.Length- 1; i >= 0; i--)
+			{
+				if (isDivesible(max[i],n) != 0)
+				{
+					print(max[i]);
+				}
+			}
 		}
 	}
 }
---
+++ Functional Programming/08.Custom Comparator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+namespace _08.Custom_Comparator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int[] max = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+			int[] max1 = { };
+			int[] max2 = { };
+			int p = 0, p2 = 0;
+			Func<int, int> isDivesible1 = x => x % 2;
+			Func<int, int> isDivesible2 = x => x % 2;
+			for (int i = 0; i < max.Length; i++)
+			{
+				if (isDivesible1(max[i]) != 0)
+				{
+					foreach (var number in max)
+					{
+						max1[p] = number;
+						p++;
+					}
+				}
+			}
+			for (int i = 0; i < max.Length; i++)
+			{
+				if (isDivesible2(max[i]) == 0)
+				{
+					foreach (var number in max)
+					{
+						max2[p2] = number;
+						p++;
+					}
+				}
+			}
+			int[] max3 = max1.Concat(max2).ToArray();
+			for (int i = 0; i < max3.Length; i++)
+			{
+				Console.WriteLine(max3[i]);
+			}
+		}
+	}
+}
