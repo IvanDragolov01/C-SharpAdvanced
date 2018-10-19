9d5443408d55bdee1cb06b268a964473c790c7a8
IvanDragolov <sinkara23@abv.bg>
2018-10-19 12:04:43 +0300

    add ex 1 and ex 2

--- Exam Preparation/01.Regeh/Program.cs
+++ Exam Preparation/01.Regeh/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace _01.Regeh
 {
@@ -6,7 +8,23 @@ namespace _01.Regeh
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string pattern = @"\[[a-zA-Z]+<(\d+)REGEH(\d+)>[a-zA-Z]+\]";
+			var input = Console.ReadLine();
+			var regex = new Regex(pattern);
+			var indexes = new List<int>();
+			foreach (Match match in regex.Matches(input))
+			{
+				var groupsValue = match.Groups[1].ToString();
+				indexes.Add(int.Parse(match.Groups[1].Value));
+				indexes.Add(int.Parse(match.Groups[2].Value));
+			}
+			int currentIndex = 0;
+			foreach (var index in indexes)
+			{
+				currentIndex += index;
+				var charIndex = currentIndex % (input.Length - 1);
+				Console.Write(input[charIndex]);
+			}
 		}
 	}
 }
