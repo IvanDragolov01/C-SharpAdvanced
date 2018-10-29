e033ea65a0323f2e6244065f6976743bb4d8180b
IvanDragolov <sinkara23@abv.bg>
2018-10-29 10:17:03 +0200

    add ex 6 and ex 7

--- Sets and Dictionaries Advanced - Exercicse/06.Wardrobe/Program.cs
+++ Sets and Dictionaries Advanced - Exercicse/06.Wardrobe/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _06.Wardrobe
 {
@@ -6,7 +8,52 @@ namespace _06.Wardrobe
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int n = int.Parse(Console.ReadLine());
+			var clothes = new Dictionary<string, Dictionary<string, int>>();
+
+			for (int i = 0; i < n; i++)
+			{
+				string[] input = Console.ReadLine().Split(" -> ");
+				string color = input[0];
+				string[] clothesstr = input[1].Split(',');
+
+				if (!clothes.ContainsKey(color))
+				{
+					clothes.Add(color, new Dictionary<string, int>());
+				}
+
+				for (int j = 0; j < clothesstr.Length; j++)
+				{
+					string currentItem = clothesstr[j];
+
+					if (!clothes[color].ContainsKey(currentItem))
+					{
+						clothes[color].Add(currentItem, 0);
+					}
+
+					clothes[color][currentItem] += 1;
+				}
+			}
+
+			string[] targetItem = Console.ReadLine().Split();
+			string targetColor = targetItem[0];
+			string itemName = targetItem[1];
+
+			foreach (var kvp in clothes)
+			{
+				Console.WriteLine($"{kvp.Key} clothes:");
+				foreach (var item in kvp.Value)
+				{
+					Console.Write($"* {item.Key} - {item.Value}");
+
+					if (kvp.Key == targetColor && item.Key == itemName)
+					{
+						Console.Write(" (found!)");
+					}
+
+					Console.WriteLine();
+				}
+			}
 		}
 	}
 }
---
+++ Sets and Dictionaries Advanced - Exercicse/07.The V-Logger/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.The_V_Logger
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			var vloggers = new Dictionary<string, Dictionary<string, SortedSet<string>>>();
+			string input;
+			while ((input = Console.ReadLine()) != "Statistics")
+			{
+				string[] elements = input.Split();
+				string user = elements[0];
+				string command = elements[1];
+				string targetUser = elements[2];
+
+				if (command == "joined")
+				{
+					if (!vloggers.ContainsKey(user))
+					{
+						vloggers.Add(user, new Dictionary<string, SortedSet<string>>());
+						vloggers[user].Add("following", new SortedSet<string>());
+						vloggers[user].Add("followers", new SortedSet<string>());
+					}
+				}
+				else if (command == "followed")
+				{
+					bool isSameVloggers = user == targetUser;
+
+					if (vloggers.ContainsKey(user) && vloggers.ContainsKey(targetUser) && !isSameVloggers)
+					{
+						vloggers[user]["following"].Add(targetUser);
+						vloggers[targetUser]["followers"].Add(user);
+					}
+				}
+			}
+
+			Console.WriteLine($"The V-Logger has a total of {vloggers.Count} vloggers in its logs.");
+
+			var sortedVloggers = vloggers.OrderByDescending(x => x.Value["followers"].Count)
+													.ThenBy(x => x.Value["following"].Count);
+			int counter = 1;
+
+			foreach (var item in sortedVloggers)
+			{
+				Console.WriteLine($"{counter}. {item.Key} : {item.Value["followers"].Count} followers, {item.Value["following"].Count} following");
+
+				if (counter == 1)
+				{
+					foreach (var followerName in item.Value["followers"])
+					{
+						Console.WriteLine($"*  {followerName}");
+					}
+				}
+
+				counter++;
+			}
+		}
+	}
+}
